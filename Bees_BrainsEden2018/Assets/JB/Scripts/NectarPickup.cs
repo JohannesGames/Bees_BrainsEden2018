c85349b47716b44ed8a883898166be370d05b820
Unknown <j.brunn@hotmail.com>
2018-07-14 12:13:43 +0100

    FOV change + prefabs

---
+++ Bees_BrainsEden2018/Assets/JB/Scripts/NectarPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NectarPickup : MonoBehaviour
+{
+    public float checkRatePerSecond = 5;
+    public float radius = 5;
+    float checkTime;
+    Collider[] pcChecker;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time >= checkTime)
+        {
+            checkTime = Time.time + 1 / checkRatePerSecond;
+            pcChecker = Physics.OverlapSphere(transform.position, radius, GameManager.gm.pcLayer);
+
+            if (pcChecker.Length > 0)
+            {
+                // if PC detected
+                GameManager.gm.pc.OnNectarPickup();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
